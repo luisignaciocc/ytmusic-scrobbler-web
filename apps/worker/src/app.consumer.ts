e31891390d73be96de28d506e3e7db039f706011
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-14 17:22:00 -0400

    add prisma to workers

--- apps/worker/src/app.consumer.ts
+++ apps/worker/src/app.consumer.ts
@@ -1,11 +1,18 @@
 import { Processor, Process } from '@nestjs/bull';
-import { Logger } from '@nestjs/common';
+import { Logger, OnModuleInit } from '@nestjs/common';
 import { Job } from 'bull';
+import { PrismaService } from './prisma.service';
 
 @Processor('scrobbler')
-export class AppConsumer {
-  constructor() {}
+export class AppConsumer implements OnModuleInit {
+  constructor(private readonly prisma: PrismaService) {}
   private readonly logger = new Logger(AppConsumer.name);
+
+  async onModuleInit() {
+    this.logger.debug(`connecting to prisma...`);
+    await this.prisma.$connect();
+    this.logger.debug(`connected`);
+  }
 
   @Process('scrobble')
   async scrobble(
@@ -15,5 +22,13 @@ export class AppConsumer {
   ) {
     const { userId } = job.data;
     this.logger.debug(`Scrobbling for user ${userId}`);
+    const users = await this.prisma.user.findFirst();
+
+    if (!users) {
+      this.logger.error(`User not found: ${userId}`);
+      return;
+    } else {
+      this.logger.debug(`User found: ${users.email}`);
+    }
   }
 }
--- apps/worker/src/app.producer.ts
+++ apps/worker/src/app.producer.ts
@@ -1,22 +1,15 @@
 import { InjectQueue } from '@nestjs/bull';
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Queue } from 'bull';
-import { PrismaService } from './prisma.service';
 
 @Injectable()
-export class AppProducer implements OnModuleInit {
+export class AppProducer {
   constructor(
-    private readonly prisma: PrismaService,
-
     @InjectQueue('scrobbler')
     private readonly queue: Queue,
   ) {}
   private readonly logger = new Logger(AppProducer.name);
-
-  async onModuleInit() {
-    await this.prisma.$connect();
-  }
 
   @Cron(CronExpression.EVERY_5_MINUTES, {
     name: 'scrobble',
