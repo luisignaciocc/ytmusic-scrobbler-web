407122410cf5d7a1331913a04bfabc1462daed9f
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-14 16:24:09 -0400

    add redis

--- apps/workers/src/app.module.ts
+++ apps/workers/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppProducer } from './app.producer';
@@ -10,15 +10,11 @@ import { AppConsumer } from './app.consumer';
   imports: [
     ConfigModule.forRoot(),
     ScheduleModule.forRoot(),
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        redis: {
-          host: configService.get('REDIS_HOST'),
-          port: configService.get('REDIS_PORT'),
-        },
-      }),
-      inject: [ConfigService],
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
     }),
     BullModule.registerQueue({
       name: 'scrobbler',
