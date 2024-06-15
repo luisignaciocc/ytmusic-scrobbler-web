0098e67b304fe7c0797c9dc39b72bcbc4f24e4aa
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-15 17:35:10 -0400

    add producer

--- apps/worker/src/app.producer.ts
+++ apps/worker/src/app.producer.ts
@@ -1,22 +1,45 @@
 import { InjectQueue } from "@nestjs/bull";
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
 import { Queue } from "bull";
 
+import { PrismaService } from "./prisma.service";
+
 @Injectable()
-export class AppProducer {
+export class AppProducer implements OnModuleInit {
   constructor(
     @InjectQueue("scrobbler")
     private readonly queue: Queue,
+    private readonly prisma: PrismaService,
   ) {}
   private readonly logger = new Logger(AppProducer.name);
+
+  async onModuleInit() {
+    this.logger.debug(`connecting to prisma...`);
+    await this.prisma.$connect();
+    this.logger.debug(`connected`);
+  }
 
   @Cron(CronExpression.EVERY_5_MINUTES, {
     name: "scrobble",
   })
-  scrobble() {
-    this.queue.add("scrobble", {
-      userId: "string",
+  async scrobble() {
+    const activeUsers = await this.prisma.user.findMany({
+      where: {
+        isActive: true,
+      },
+      select: {
+        id: true,
+      },
     });
+
+    this.queue.addBulk(
+      activeUsers.map((user) => ({
+        name: "scrobble",
+        data: {
+          userId: user.id,
+        },
+      })),
+    );
   }
 }
