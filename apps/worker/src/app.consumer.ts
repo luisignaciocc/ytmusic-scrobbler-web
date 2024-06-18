2c7f978729fa756e1c4a19ded7ce19192d310dd7
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-18 19:32:22 -0400

    improve readability

--- apps/worker/src/app.consumer.ts
+++ apps/worker/src/app.consumer.ts
@@ -1,11 +1,14 @@
 import { Process, Processor } from "@nestjs/bull";
 import { Logger, OnModuleInit } from "@nestjs/common";
 import { Job } from "bull";
-import { parseStringPromise } from "xml2js";
 
 import { PrismaService } from "./prisma.service";
-import hashRequest from "./utils/functions";
-import { LastFmScrobbleResponse, YTAPIResponse } from "./utils/types";
+import {
+  getGoogleVisitorId,
+  getNewGoogleToken,
+  getYTMusicHistory,
+  scrobbleSong,
+} from "./utils/functions";
 @Processor("scrobbler")
 export class AppConsumer implements OnModuleInit {
   constructor(private readonly prisma: PrismaService) {}
@@ -53,20 +56,7 @@ export class AppConsumer implements OnModuleInit {
     }
 
     try {
-      const res = await fetch("https://music.youtube.com", {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        },
-      });
-      const text = await res.text();
-      const matches = text.match(/ytcfg\.set\s*\(\s*({.+?})\s*\)\s*;/);
-      let visitorId = "";
-
-      if (matches && matches.length > 0) {
-        const ytcfg = JSON.parse(matches[1]);
-        visitorId = ytcfg.VISITOR_DATA || "";
-      }
+      const visitorId = await getGoogleVisitorId();
 
       if (!user.lastFmSessionKey) {
         job.log(`User ${userId} is not authenticated with Last.fm`);
@@ -84,41 +74,23 @@ export class AppConsumer implements OnModuleInit {
         user.googleTokenExpires < new Date().getTime()
       ) {
         try {
-          const response = await fetch("https://oauth2.googleapis.com/token", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              client_id: GOOGLE_CLIENT_ID,
-              client_secret: GOOGLE_CLIENT_SECRET,
-              refresh_token: user.googleRefreshToken,
-              grant_type: "refresh_token",
-            }),
-          });
-
-          if (!response.ok) {
-            job.log(`Error refreshing user token for ${userId}`);
-            return job.discard();
-          }
-
-          const data: {
-            access_token: string;
-            expires_in: number;
-          } = await response.json();
-
-          const expires_at = new Date().getTime() + data.expires_in * 1000;
-          accessToken = data.access_token;
+          const { accessToken: newAccessToken, expiresAt } =
+            await getNewGoogleToken({
+              clientId: GOOGLE_CLIENT_ID,
+              clientSecret: GOOGLE_CLIENT_SECRET,
+              refreshToken: user.googleRefreshToken,
+            });
 
           await this.prisma.user.update({
             where: {
               googleId: user.googleId,
             },
             data: {
-              googleAccessToken: data.access_token,
-              googleTokenExpires: expires_at,
+              googleAccessToken: newAccessToken,
+              googleTokenExpires: expiresAt,
             },
           });
+          accessToken = newAccessToken;
         } catch (error) {
           if (error instanceof Error) {
             job.log(`Error refreshing user token for ${userId}`);
@@ -131,33 +103,10 @@ export class AppConsumer implements OnModuleInit {
         }
       }
 
-      const [musicResponse] = await Promise.all([
-        fetch("https://music.youtube.com/youtubei/v1/browse", {
-          method: "POST",
-          cache: "no-store",
-          headers: {
-            "Content-Type": "application/json",
-            "X-Goog-Visitor-Id": visitorId,
-            Authorization: `Bearer ${accessToken}`,
-            Cookie: "SOCS=CAI",
-            Origin: "https://music.youtube.com",
-            "User-Agent":
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            Referer: "https://music.youtube.com/",
-          },
-          body: JSON.stringify({
-            context: {
-              client: {
-                clientName: "WEB_REMIX",
-                clientVersion: "0.1",
-                hl: "en",
-                gl: "US",
-                experimentsToken: "",
-                utcOffsetMinutes: 0,
-              },
-            },
-            browseId: "FEmusic_history",
-          }),
+      const [songs] = await Promise.all([
+        getYTMusicHistory({
+          visitorId,
+          accessToken,
         }),
         // delete song older tah 24h
         this.prisma.song.deleteMany({
@@ -170,174 +119,62 @@ export class AppConsumer implements OnModuleInit {
         }),
       ]);
 
-      const data = await musicResponse.json();
-
-      const results: YTAPIResponse =
-        data.contents?.singleColumnBrowseResultsRenderer?.tabs?.[0]?.tabRenderer
-          ?.content?.sectionListRenderer?.contents;
-
-      if (!results) {
-        job.log(`No results found for user ${userId}`);
-        return job.discard();
-      }
-
-      const songs: {
-        title: string;
-        artist: string;
-        album: string;
-        playedAt?: string;
-      }[] = [];
-      results.forEach(({ musicShelfRenderer }) => {
-        if (!musicShelfRenderer) {
-          return;
-        }
-        const playedAt = musicShelfRenderer?.title?.runs?.[0]?.text;
-
-        musicShelfRenderer?.contents?.forEach(
-          ({ musicResponsiveListItemRenderer }) => {
-            if (!musicResponsiveListItemRenderer) {
-              return;
-            }
-            const flexColumns = musicResponsiveListItemRenderer?.flexColumns;
-
-            if (!flexColumns) {
-              return;
-            }
-
-            const watchEndpointFlexColumn = flexColumns.find(
-              (flexColumn) =>
-                flexColumn.musicResponsiveListItemFlexColumnRenderer?.text
-                  ?.runs?.[0]?.navigationEndpoint?.watchEndpoint,
-            );
-
-            const browseEndpointFlexColumnArtist = flexColumns.find(
-              (flexColumn) =>
-                flexColumn.musicResponsiveListItemFlexColumnRenderer?.text
-                  ?.runs?.[0]?.navigationEndpoint?.browseEndpoint
-                  ?.browseEndpointContextSupportedConfigs
-                  ?.browseEndpointContextMusicConfig?.pageType ===
-                "MUSIC_PAGE_TYPE_ARTIST",
-            );
-
-            const browseEndpointFlexColumnAlbum = flexColumns.find(
-              (flexColumn) =>
-                flexColumn.musicResponsiveListItemFlexColumnRenderer?.text
-                  ?.runs?.[0]?.navigationEndpoint?.browseEndpoint
-                  ?.browseEndpointContextSupportedConfigs
-                  ?.browseEndpointContextMusicConfig?.pageType ===
-                "MUSIC_PAGE_TYPE_ALBUM",
-            );
-
-            if (!watchEndpointFlexColumn || !browseEndpointFlexColumnArtist) {
-              return;
-            }
-
-            const title =
-              watchEndpointFlexColumn.musicResponsiveListItemFlexColumnRenderer
-                ?.text?.runs?.[0]?.text;
-            const artist =
-              browseEndpointFlexColumnArtist
-                .musicResponsiveListItemFlexColumnRenderer?.text?.runs?.[0]
-                ?.text;
-            const album =
-              browseEndpointFlexColumnAlbum
-                ?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.[0]
-                ?.text || title;
-
-            if (title && artist) {
-              songs.push({
-                title,
-                artist,
-                album: album!,
-                playedAt,
-              });
-            }
-          },
-        );
-      });
-
       let songsReproducedToday = 0;
-      songs
-        .filter((song) => song.playedAt === "Today")
-        .forEach(async (song, index) => {
-          songsReproducedToday++;
-          try {
-            const savedSong = await this.prisma.song.findFirst({
-              where: {
-                title: song.title,
-                artist: song.artist,
-                album: song.album,
-                userId: user.id,
-              },
-            });
-            if (!savedSong) {
-              const url = new URL("http://ws.audioscrobbler.com/2.0/");
-
-              const params = {
-                album: song.album,
-                api_key: LAST_FM_API_KEY,
-                method: "track.scrobble",
+      let songsScrobbled = 0;
+      for await (const song of songs.filter(
+        (song) => song.playedAt === "Today",
+      )) {
+        songsReproducedToday++;
+        try {
+          const savedSong = await this.prisma.song.findFirst({
+            where: {
+              title: song.title,
+              artist: song.artist,
+              album: song.album,
+              userId: user.id,
+            },
+          });
+          if (!savedSong) {
+            await Promise.all([
+              scrobbleSong({
+                song,
+                lastFmApiKey: LAST_FM_API_KEY,
+                lastFmApiSecret: LAST_FM_API_SECRET,
+                lastFmSessionKey: user.lastFmSessionKey!,
                 timestamp: (
                   Math.floor(new Date().getTime() / 1000) -
-                  30 * (index + 1)
+                  30 * (songsScrobbled + 1)
                 ).toString(),
-                track: song.title,
-                artist: song.artist,
-                sk: user.lastFmSessionKey!,
-              };
+              }),
+              this.prisma.song.create({
+                data: {
+                  title: song.title,
+                  artist: song.artist,
+                  album: song.album,
+                  addedAt: new Date(),
+                  userId: user.id,
+                },
+              }),
+            ]);
 
-              const requestHash = hashRequest(params, LAST_FM_API_SECRET);
-
-              const urlParams = new URLSearchParams(params);
-              urlParams.append("api_sig", requestHash);
-
-              url.search = urlParams.toString();
-
-              const res = await fetch(url, {
-                method: "POST",
-              });
-
-              const text = await res.text();
-
-              const result: LastFmScrobbleResponse =
-                await parseStringPromise(text);
-
-              const scrobbles = result.lfm.scrobbles?.[0].$;
-              const accepted = scrobbles?.accepted;
-              const ignored = scrobbles?.ignored;
-
-              if (accepted === "0" && ignored === "0") {
-                job.log("Error scrobbling song " + song.title);
-              } else if (accepted === "0") {
-                job.log(
-                  "Song scrobble was ignored " +
-                    result.lfm.scrobbles?.[0].scrobble[0].ignoredMessage,
-                );
-              } else {
-                job.log(
-                  `Scrobbled song ${song.title} by ${song.artist} - user ${userId}`,
-                );
-                await this.prisma.song.create({
-                  data: {
-                    title: song.title,
-                    artist: song.artist,
-                    album: song.album,
-                    addedAt: new Date(),
-                    userId: user.id,
-                  },
-                });
-              }
-            }
-          } catch (error) {
-            job.log(`Error scrobbling song for user ${userId}`);
-            job.log(error);
-            return job.discard();
+            songsScrobbled++;
+            job.log(
+              `Scrobbled song ${song.title} by ${song.artist} - user ${userId}`,
+            );
           }
-        });
+        } catch (error) {
+          job.log(`Error scrobbling song for user ${userId}`);
+          job.log(error);
+          return job.discard();
+        }
+      }
+
       this.logger.debug(`Scrobbling for user ${userId} done at ${new Date()}`);
       await job.progress(100);
+
       return {
         songsReproducedToday,
+        songsScrobbled,
       };
     } catch (error) {
       job.log(`Error scrobbling for user ${userId}`);
--- apps/worker/src/utils/functions.ts
+++ apps/worker/src/utils/functions.ts
@@ -1,9 +1,9 @@
 import * as crypto from "crypto";
+import { parseStringPromise } from "xml2js";
 
-export default function hashRequest(
-  params: Record<string, string>,
-  secretKey: string,
-) {
+import { LastFmScrobbleResponse, YTAPIResponse } from "./types";
+
+export function hashRequest(params: Record<string, string>, secretKey: string) {
   let string = "";
   const keys = Object.keys(params).sort();
   keys.forEach((key) => {
@@ -12,4 +12,242 @@ export default function hashRequest(
   });
   string += secretKey;
   return crypto.createHash("md5").update(string, "utf8").digest("hex");
+}
+
+export async function getNewGoogleToken({
+  clientId,
+  clientSecret,
+  refreshToken,
+}: {
+  clientId: string;
+  clientSecret: string;
+  refreshToken: string;
+}) {
+  const response = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      client_id: clientId,
+      client_secret: clientSecret,
+      refresh_token: refreshToken,
+      grant_type: "refresh_token",
+    }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error);
+  }
+
+  const data: {
+    access_token: string;
+    expires_in: number;
+  } = await response.json();
+
+  const expiresAt = new Date().getTime() + data.expires_in * 1000;
+  const accessToken = data.access_token;
+
+  return { accessToken, expiresAt };
+}
+
+export async function getGoogleVisitorId() {
+  const res = await fetch("https://music.youtube.com", {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch visitor id");
+  }
+
+  const text = await res.text();
+  const matches = text.match(/ytcfg\.set\s*\(\s*({.+?})\s*\)\s*;/);
+
+  if (matches && matches.length > 0) {
+    const ytcfg = JSON.parse(matches[1]);
+    return ytcfg.VISITOR_DATA || "";
+  }
+}
+
+export async function getYTMusicHistory({
+  visitorId,
+  accessToken,
+}: {
+  visitorId: string;
+  accessToken: string;
+}) {
+  const musicResponse = await fetch(
+    "https://music.youtube.com/youtubei/v1/browse",
+    {
+      method: "POST",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Visitor-Id": visitorId,
+        Authorization: `Bearer ${accessToken}`,
+        Cookie: "SOCS=CAI",
+        Origin: "https://music.youtube.com",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        Referer: "https://music.youtube.com/",
+      },
+      body: JSON.stringify({
+        context: {
+          client: {
+            clientName: "WEB_REMIX",
+            clientVersion: "0.1",
+            hl: "en",
+            gl: "US",
+            experimentsToken: "",
+            utcOffsetMinutes: 0,
+          },
+        },
+        browseId: "FEmusic_history",
+      }),
+    },
+  );
+
+  const data = await musicResponse.json();
+
+  const results: YTAPIResponse =
+    data.contents?.singleColumnBrowseResultsRenderer?.tabs?.[0]?.tabRenderer
+      ?.content?.sectionListRenderer?.contents;
+
+  if (!results) {
+    throw new Error("Failed to fetch music history");
+  }
+
+  const songs: {
+    title: string;
+    artist: string;
+    album: string;
+    playedAt?: string;
+  }[] = [];
+
+  results.forEach(({ musicShelfRenderer }) => {
+    if (!musicShelfRenderer) {
+      return;
+    }
+    const playedAt = musicShelfRenderer?.title?.runs?.[0]?.text;
+
+    musicShelfRenderer?.contents?.forEach(
+      ({ musicResponsiveListItemRenderer }) => {
+        if (!musicResponsiveListItemRenderer) {
+          return;
+        }
+        const flexColumns = musicResponsiveListItemRenderer?.flexColumns;
+
+        if (!flexColumns) {
+          return;
+        }
+
+        const watchEndpointFlexColumn = flexColumns.find(
+          (flexColumn) =>
+            flexColumn.musicResponsiveListItemFlexColumnRenderer?.text
+              ?.runs?.[0]?.navigationEndpoint?.watchEndpoint,
+        );
+
+        const browseEndpointFlexColumnArtist = flexColumns.find(
+          (flexColumn) =>
+            flexColumn.musicResponsiveListItemFlexColumnRenderer?.text
+              ?.runs?.[0]?.navigationEndpoint?.browseEndpoint
+              ?.browseEndpointContextSupportedConfigs
+              ?.browseEndpointContextMusicConfig?.pageType ===
+            "MUSIC_PAGE_TYPE_ARTIST",
+        );
+
+        const browseEndpointFlexColumnAlbum = flexColumns.find(
+          (flexColumn) =>
+            flexColumn.musicResponsiveListItemFlexColumnRenderer?.text
+              ?.runs?.[0]?.navigationEndpoint?.browseEndpoint
+              ?.browseEndpointContextSupportedConfigs
+              ?.browseEndpointContextMusicConfig?.pageType ===
+            "MUSIC_PAGE_TYPE_ALBUM",
+        );
+
+        if (!watchEndpointFlexColumn || !browseEndpointFlexColumnArtist) {
+          return;
+        }
+
+        const title =
+          watchEndpointFlexColumn.musicResponsiveListItemFlexColumnRenderer
+            ?.text?.runs?.[0]?.text;
+        const artist =
+          browseEndpointFlexColumnArtist
+            .musicResponsiveListItemFlexColumnRenderer?.text?.runs?.[0]?.text;
+        const album =
+          browseEndpointFlexColumnAlbum
+            ?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.[0]
+            ?.text || title;
+
+        if (title && artist) {
+          songs.push({
+            title,
+            artist,
+            album: album!,
+            playedAt,
+          });
+        }
+      },
+    );
+  });
+
+  return songs;
+}
+
+export async function scrobbleSong({
+  song,
+  lastFmSessionKey,
+  lastFmApiKey,
+  lastFmApiSecret,
+  timestamp,
+}: {
+  song: { title: string; artist: string; album: string };
+  lastFmSessionKey: string;
+  lastFmApiKey: string;
+  lastFmApiSecret: string;
+  timestamp: string;
+}) {
+  const url = new URL("http://ws.audioscrobbler.com/2.0/");
+
+  const params = {
+    album: song.album,
+    api_key: lastFmApiKey,
+    method: "track.scrobble",
+    timestamp,
+    track: song.title,
+    artist: song.artist,
+    sk: lastFmSessionKey,
+  };
+
+  const requestHash = hashRequest(params, lastFmApiSecret);
+
+  const urlParams = new URLSearchParams(params);
+  urlParams.append("api_sig", requestHash);
+
+  url.search = urlParams.toString();
+
+  const res = await fetch(url, {
+    method: "POST",
+  });
+
+  const text = await res.text();
+
+  const result: LastFmScrobbleResponse = await parseStringPromise(text);
+
+  const scrobbles = result.lfm.scrobbles?.[0].$;
+  const accepted = scrobbles?.accepted;
+  const ignored = scrobbles?.ignored;
+
+  if (accepted === "0" && ignored === "0") {
+    throw new Error("Error scrobbling song " + song.title);
+  } else if (accepted === "0") {
+    throw new Error("Song scrobble was ignored " + song.title);
+  } else {
+    return true;
+  }
 }
