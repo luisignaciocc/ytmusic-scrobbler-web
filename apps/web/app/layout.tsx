c887ecdc59e7d3a6ff878cfd6494144f606cc4d1
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-25 10:58:36 -0400

    add analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <SessionAuthProvider>{children}</SessionAuthProvider>
+        <Analytics />
       </body>
     </html>
   );
