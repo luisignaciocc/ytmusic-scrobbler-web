9921bf2b868953419c68fb7910ad80d5c63b1c60
Ignacio Collantes <luisignacioccp@gmail.com>
2025-03-16 11:03:13 -0300

    chore: comment out deployment steps in GitHub Actions workflow and simplify user session handling in get-server-session.ts

--- apps/web/lib/get-server-session.ts
+++ apps/web/lib/get-server-session.ts
@@ -17,51 +17,34 @@ export const authConfig = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          // prompt: "consent", // Uncomment to force the consent screen to appear every time
-          access_type: "offline",
           response_type: "code",
-          scope:
-            "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid",
+          scope: "openid email profile",
         },
       },
     }),
   ],
   callbacks: {
-    jwt: async ({ token, user, account }) => {
+    jwt: async ({ token, user }) => {
       if (user) {
         token.user = user;
       }
-      if (account && user) {
+      if (user?.email) {
         const email = user.email;
-        const name = user.name || email?.split("@")[0];
-        const googleId = account.providerAccountId;
+        const name = user.name || email.split("@")[0];
         const picture = user.image || "";
-        const googleAccessToken = account.access_token;
-        const googleRefreshToken = account.refresh_token;
-        const googleTokenExpires = account.expires_at;
-        const googleIdToken = account.id_token;
-        if (email && name && googleAccessToken) {
-          await prisma.user.upsert({
-            where: { email },
-            create: {
-              email,
-              name,
-              googleId,
-              picture,
-              googleAccessToken,
-              googleRefreshToken: googleRefreshToken || undefined,
-              googleTokenExpires,
-              googleIdToken,
-            },
-            update: {
-              name,
-              picture,
-              googleAccessToken,
-              googleTokenExpires,
-              googleIdToken,
-            },
-          });
-        }
+
+        await prisma.user.upsert({
+          where: { email },
+          create: {
+            email,
+            name,
+            picture,
+          },
+          update: {
+            name,
+            picture,
+          },
+        });
       }
       return token;
     },
