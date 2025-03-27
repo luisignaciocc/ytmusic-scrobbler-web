4064068c37f07df2e3bbc85a9f53666020cb40dc
Ignacio Collantes <luisignacioccp@gmail.com>
2025-03-27 14:04:33 -0300

    fix: adjust padding in main section for improved layout consistency

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
 
   return (
     <main className="flex-1">
-      <section className="w-full py-8 md:py-16 lg:py-24 xl:py-40">
+      <section className="w-full py-4 md:py-8 lg:py-16 xl:py-24">
         <div className="container px-4 md:px-6 mx-auto">
           <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px] items-center">
             <div className="flex flex-col justify-center space-y-4">
