f3c36ed85e7babffa70e7ae06c5474a58101ef72
Ignacio Collantes <luisignacioccp@gmail.com>
2025-04-06 11:21:22 -0400

    refactor: update user deactivation logic to include users with null lastSuccessfulScrobble and createdAt older than seven days

--- apps/web/app/api/cron/deactivate/route.ts
+++ apps/web/app/api/cron/deactivate/route.ts
@@ -3,11 +3,23 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
+  const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+
   await prisma.user.updateMany({
     where: {
-      lastSuccessfulScrobble: {
-        lt: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
-      },
+      OR: [
+        {
+          lastSuccessfulScrobble: {
+            lt: sevenDaysAgo,
+          },
+        },
+        {
+          lastSuccessfulScrobble: null,
+          createdAt: {
+            lt: sevenDaysAgo,
+          },
+        },
+      ],
     },
     data: {
       isActive: false,
