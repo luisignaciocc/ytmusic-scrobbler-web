c16bd662aa881d5dd425a03c7bc0a16111e188f8
Luis Ignacio <luisignacioccp@gmail.com>
2025-09-13 20:07:04 -0300

    fix: Optimize Sentry configuration to reduce memory usage during builds

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,36 +3,18 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// Make sure adding Sentry options is the last code to run before exporting
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// Disable Sentry webpack processing entirely during build to save memory
+const isBuild = process.env.NODE_ENV === 'production' || process.argv.includes('build');
 
-  org: "bocono",
-
-  project: "ytmusic-scrobbler-web",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+export default isBuild
+  ? nextConfig // Skip Sentry entirely during builds
+  : withSentryConfig(nextConfig, {
+      org: "bocono",
+      project: "ytmusic-scrobbler-web",
+      silent: true,
+      widenClientFileUpload: false,
+      sourcemaps: { disable: true },
+      disableLogger: true,
+      automaticVercelMonitors: false,
+      skipRelease: true,
+    });
